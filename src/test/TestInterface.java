1f4b7f8a3af2444b44adb07c1406f13b490a5ba6
ashwini <ashwini1065@gmail.com>
2021-07-11 07:22:41 +0000

    11jul3

--- src/test/TestInterface.java
+++ src/test/TestInterface.java
@@ -4,15 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class TestInterface {
+public interface TestInterface {
 	
 	public void method1();
 	
 	@Test
-	default void test1()
-	
-	{
+	default void test1() {
+		
 		assertTrue(Stringfunctions.isPalindrome("madam"));
 	}
+
 
 }
--- src/test/TestinterfaceImplementation.java
+++ src/test/TestinterfaceImplementation.java
@@ -1,6 +1,19 @@
 package test;
 
-public class TestinterfaceImplementation implements TestInterface{
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestinterfaceImplementation implements TestInterface {
+
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Inside Class");
+		
+	}
 	
 	
+
 }
